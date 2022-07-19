57d3efdcad33b1a7f96169c42da40b7aa3e383a9
AMARJITH V <amarjith.v@bridge-global.com>
2022-07-19 14:14:03 +0530

    netlify adapter added

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: undefined,
+		adapter: adapter()
 	},
 
 	// options passed to svelte.preprocess (https://svelte.dev/docs#compile-time-svelte-preprocess)
